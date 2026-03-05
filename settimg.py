7544430eb795e0747b7edae7bcd9da03e7ad6f3e
ihorvitko <ihor.vitko.dev@gmail.com>
2026-03-05 22:19:54 +0200

    init: project structure

---
+++ settimg.py
@@ -0,0 +1,10 @@
+from pydantic_settings import BaseSettings
+from pydantic import ConfigDict
+
+
+class Config(BaseSettings):
+    model_config = ConfigDict(env_file=".env")
+
+    BOT_TOKEN: str
+    ChatGPT_TOKEN: str
+    DEBUG: bool = False
